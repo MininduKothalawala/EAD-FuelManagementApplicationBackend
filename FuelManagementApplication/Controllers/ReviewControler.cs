80b4691abba9eb6211445b901443223b2d75062a
Minindu Kothalawala <it19184036@my.sliit.lk>
2022-10-25 01:03:15 +0530

    contoller classes changed

--- FuelManagementApplication/Controllers/ReviewControler.cs
+++ FuelManagementApplication/Controllers/ReviewControler.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace FuelManagementApplication.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/ReviewController/")]
     public class ReviewControler : ControllerBase
     {
         private readonly IReviewRepository reviewRepository;
